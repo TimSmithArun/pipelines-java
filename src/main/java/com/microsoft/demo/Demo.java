9c023ec23d5201a2a88aaa728741a1186b948ee5
TimSmithArun <48550341+TimSmithArun@users.noreply.github.com>
2019-07-16 09:30:25 +0100

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -1,7 +1,7 @@
 package com.microsoft.demo;
 
 public class Demo {
-    public void DoSomething(boolean flag){
+    public viod DoSomething(boolean flag){
         if(flag){
             System.out.println("I am covered");
             return;
